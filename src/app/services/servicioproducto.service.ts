58515ebebf4ba31aab0b3744fc1aae0c1ac4aed5
Gerald Gonzalez <gedgonz7@gmail.com>
2019-11-26 22:32:39 -0600

    ajustes de configuracion

--- src/app/services/servicioproducto.service.ts
+++ src/app/services/servicioproducto.service.ts
@@ -10,36 +10,37 @@ export class ServicioproductoService {
 
   urlNetCore:string = 'https://localhost:5001/api/Producto';
   urlNodeJS: string = 'http://localhost:3000/api/Producto';
+  uri: string = 'https://marketfunkopop.herokuapp.com/api/Producto';
   constructor(private http: HttpClient) {
   }
 
 
   obtenerProductos(): Observable<producto[]> {
-    const url = this.urlNetCore;
+    const url = this.uri;
     return this.http.get<producto[]>(url);
   }
 
   obtenerProducto(_id: number): Observable<producto> {
-    const url = `${this.urlNetCore}/${_id}`;
+    const url = `${this.uri}/${_id}`;
     console.log(url);
     return this.http.get<producto>(url);
   }
 
   nuevoProducto(_producto: producto){
     console.log(_producto);
-    const url = this.urlNetCore;
+    const url = this.uri;
     return this.http.post(url, _producto);
   }
 
   actualizarProducto(_producto: producto){
     console.log(_producto);
-    const url = `${this.urlNetCore}/${_producto._id}`;
+    const url = `${this.uri}/${_producto._id}`;
     return this.http.put(url, _producto);
   }
 
   eliminarProducto(_producto: producto){
     console.log(_producto);
-    const url = `${this.urlNetCore}/${_producto._id}`;
+    const url = `${this.uri}/${_producto._id}`;
     return this.http.delete(url);
   }
 }
